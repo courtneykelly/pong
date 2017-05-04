3d586684b993ca985800e03ad25fadb79c18db2a
Katie Schermerhorn <katieschermerhorn@katies-mbp.dhcp.nd.edu>
2017-05-04 16:34:41 -0400

    setting up gamespace

---
+++ GameSpace.py
@@ -0,0 +1,60 @@
+import pygame
+import math
+
+class GameSpace:
+	def main(self):
+
+		# Initialize screen
+		pygame.init()
+		self.size = self.width, self.height = 640, 470
+		self.black = 0, 0, 0
+		self.screen = pygame.display.set_mode(self.size)
+
+		# Initialize objects and clock
+		self.spriteList = []
+		self.player1 = Player(self, img1)
+		self.player2 = Player(self, img2)
+		self.spriteList.append(self.player1)
+		self.spriteList.append(self.player2)
+		# self.lasers = []
+		self.clock = pygame.time.Clock()
+		# self.explosion = 0
+
+
+		while 1:
+			self.clock.tick(60)
+			for event in pygame.event.get():
+
+				# If the user clicks out of the game
+				if event.type == pygame.QUIT:
+					exit(0)
+
+				# If down or up arrow key is pressed
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_UP:
+						self.player.move([0,-5])
+					elif event.key == pygame.K_DOWN:
+						self.player.move([0,5])
+				elif event.type == pygame.KEYUP:
+					if event.key == pygame.K_UP:
+						pass
+					elif event.key == pygame.K_DOWN:
+						pass
+
+
+				# If the user shoots a laser
+				if event.type == pygame.MOUSEBUTTONDOWN:
+					if self.player.is_firing == False:
+						self.player.is_firing = True
+						self.player.mouse_x, self.player.mouse_y = pygame.mouse.get_pos()
+
+			for sprite in self.spriteList:
+				sprite.tick()
+
+			self.screen.fill(self.black)
+
+			for sprite in reversed(self.spriteList):
+				self.screen.blit(sprite.img, sprite.rect)
+
+
+			pygame.display.flip()
---
+++ main.py
@@ -0,0 +1,6 @@
+import pygame
+import math
+
+if __name__ == "__main__":
+	gs = GameSpace()
+	gs.main()
