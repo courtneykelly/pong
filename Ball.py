721abed062804d9a7aff217c86af69a7402862db
Katie Schermerhorn <katieschermerhorn@katies-mbp.dhcp.nd.edu>
2017-05-05 12:06:06 -0400

    adding score

--- Ball.py
+++ Ball.py
@@ -1,39 +1,55 @@
 import pygame
 import math
+import random
 
 class Ball(pygame.sprite.Sprite):
 	def __init__(self, gs):
 		self.gs = gs
-		self.img = pygame.image.load("sprites/player.png")
+		self.img = pygame.image.load("sprites/ball.png")
 		self.rect = self.img.get_rect()
+		self.rect.centerx = self.gs.width/2
+		self.rect.centery = self.gs.height/2
 
-		self.move_speed = 0
+		self.speeds = [-3, 3]
+		self.move_speed_x = random.choice(self.speeds)
+		self.move_speed_y = random.choice(self.speeds)
 
 
-	def move(self, coord):
-		self.rect = self.rect.move(coord)
+	def move(self):
+		self.rect.centerx += self.move_speed_x
+		self.rect.centery += self.move_speed_y
 
-	# def rotate(self, degree):
-	# 	self.img = pygame.transform.rotate(self.org_img, degree)
-	# 	self.rect = self.img.get_rect(center=self.rect.center)
+	def reset(self):
+		self.rect.centerx = self.gs.width/2
+		self.rect.centery = self.gs.height/2
+		self.move_speed_x = random.choice(self.speeds)
+		self.move_speed_y = random.choice(self.speeds)		
 
 	def tick(self):
-		if self.rect.centery+self.move_speed >= 0 and self.rect.centery+self.move_speed <= self.gs.height:
-			self.rect.centery += self.move_speed
-	# 	if self.is_firing:
 
-	# 		angle = math.atan2(self.rect.centery-self.mouse_y, self.mouse_x-self.rect.centerx)
-	# 		laser = Laser(self.gs, angle)
-	# 		self.gs.spriteList.append(laser)
-	# 		self.gs.lasers.append(laser)
-	# 		self.time_to_fire+=1
+		if ( self.rect.centerx + 15 < 0 ):
+			self.gs.score.increment_score(2)
+			self.reset()
+		elif ( self.rect.centerx - 15 > self.gs.width ):
+			self.gs.score.increment_score(1)
+			self.reset()
 
-	# 		if self.time_to_fire == 30:
-	# 			self.is_firing = False
-	# 			self.time_to_fire = 0
-	# 	else:
-	# 		mousex, mousey = pygame.mouse.get_pos()
-	# 		playerx = self.rect.centerx
-	# 		playery = self.rect.centery
-	# 		degree = math.degrees(math.atan2(mousex-playerx, mousey-playery)) + 225
-	# 		self.rotate(degree)
+		# Collisions with bar
+		if ( self.rect.colliderect(self.gs.player1.rect) == True):
+			self.move_speed_x = -self.move_speed_x
+			self.move()
+		elif ( self.rect.colliderect(self.gs.player2.rect) == True):
+			self.move_speed_x = -self.move_speed_x
+			self.move()
+		# Collisions with top or bottom
+		elif ( self.rect.centery + 20 >= self.gs.height ):
+			self.move_speed_y = -self.move_speed_y
+			self.move()
+
+		elif ( self.rect.centery - 20 <= 0 ):
+			self.move_speed_y = -self.move_speed_y
+			self.move()
+		else:
+			self.move()
+
+
