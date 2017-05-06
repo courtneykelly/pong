577a25bd1136e7bed6dddd827febb584bb03763d
Katie Schermerhorn <katieschermerhorn@katies-mbp.dhcp.nd.edu>
2017-05-06 12:40:32 -0400

    change score tick

--- Score.py
+++ Score.py
@@ -20,10 +20,9 @@ class Score(pygame.sprite.Sprite):
 
 	def increment_score(self):
 		self.score+=1
+
+	def tick(self):
 		img_link = "sprites/" + str(self.score) + ".png"
 		self.img = pygame.image.load(img_link)
 		if self.score == 10:
 			self.gs.stop = 1
-
-	def tick(self):
-		pass
