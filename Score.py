3b3350132dc4d631670428fda557eb735f372061
Katie Schermerhorn <katieschermerhorn@katies-mbp.dhcp.nd.edu>
2017-05-06 15:26:41 -0400

    by the grace of god7

--- Score.py
+++ Score.py
@@ -24,7 +24,7 @@ class Score(pygame.sprite.Sprite):
 	def tick(self):
 		img_link = "sprites/" + str(self.score) + ".png"
 		self.img = pygame.image.load(img_link)
-		if self.score == 10 and self.gs.level >= 3:
+		if self.score >= 10 and self.gs.level >= 3:
 			if self.player == 1:
 				self.gs.player1_tot += 1
 			else:
@@ -37,7 +37,7 @@ class Score(pygame.sprite.Sprite):
 			else:
 				self.gs.win = 2
 			self.score = 0
-		elif self.score == 10:
+		elif self.score >= 10:
 			if self.player == 1:
 				self.gs.player1_tot += 1
 			else:
